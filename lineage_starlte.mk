c59fbd0da06452224e777562becbcae03833d5ee
Cyder | Lumine <87647653+CyderVII28x03@users.noreply.github.com>
2022-01-26 08:48:34 +0700

    Add ProjectSakura Maintainer name.

--- lineage_starlte.mk
+++ lineage_starlte.mk
@@ -26,6 +26,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit some common LineageOS stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 TARGET_INCLUDE_PIXEL_CHARGER := true
+SAKURA_MAINTAINER := Cyder
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := starlte
